31aeb52deb67a09d5e3a1bc0900e9bcc8a210863
DiogoDking77 <diogoabreis@gmail.com>
2023-11-13 17:37:31 +0000

    s

---
+++ JavaScript/Landing_Page.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let header = document.querySelector('.tab-bar');
+    let navTabs = document.querySelector('.nav-tabs');
+    let isHamburgerMenuActive = false;
+
+    function toggleHeaderVisibility() {
+        let currentScrollPos = window.scrollY;
+
+        if (currentScrollPos === 0) {
+            header.classList.add('hidden');
+            // Restaura o estado do menu de hambúrguer ao rolar para o topo
+            if (isHamburgerMenuActive) {
+                navTabs.classList.remove('active');
+                isHamburgerMenuActive = false;
+            }
+        } else {
+            header.classList.remove('hidden');
+        }
+    }
+
+    window.toggleNavTabs = function() {
+        navTabs.classList.toggle('active');
+        isHamburgerMenuActive = !isHamburgerMenuActive;
+
+        // Adicione a lógica para ocultar a barra de navegação quando voltar ao topo
+        if (!isHamburgerMenuActive) {
+            header.classList.remove('hidden');
+        }
+    };
+
+    // Chame a função ao carregar a página para ocultar o cabeçalho inicialmente
+    toggleHeaderVisibility();
+
+    window.addEventListener('scroll', function() {
+        toggleHeaderVisibility();
+    });
+});
